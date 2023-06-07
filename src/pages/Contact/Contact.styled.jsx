9f574d12b31c8d06690f7d9f6d19b278c4b9530f
Enirose <enirosehellum@gmail.com>
2023-06-07 10:43:27 +0200

    added styling fort contact page

--- src/pages/Contact/Contact.styled.jsx
+++ src/pages/Contact/Contact.styled.jsx
@@ -1,12 +1,13 @@
 import { styled } from "styled-components";
 
 export const ContactForm = styled.form `
-    border: 3px solid#030303;
+    border: 2px solid#030303;
     display: flex;
     flex-direction: column;
     padding: 30px;
     margin: 20rem;
     margin-top: auto;
+    margin-bottom: 20rem;
 
     input {
         margin-bottom: 10px;
@@ -33,5 +34,26 @@ export const ContactForm = styled.form `
         font-weight: bold;
         text-transform: uppercase;
         cursor: pointer;
+    }
+
+    @media (max-width: 450px) {
+        margin: 0;
+        padding: 10px;
+        margin-bottom: 20rem;
+
+    }
+
+    @media (max-width: 768px) {
+        margin: 20px;
+        padding: 10px;
+        margin-bottom: 20rem;
+
+    }
+
+    @media (min-width: 1200px) {
+        margin: 2rem auto;
+        max-width: 600px;
+        margin-bottom: 20rem;
+
     }
 `
